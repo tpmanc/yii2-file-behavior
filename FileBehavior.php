6f5620add2cd621d02a88e7c21fa1b667c37e8b3
tpmanc <tpxtrime@mail.ru>
2015-07-30 15:54:12 +0300

    Update FileBehavior.php

--- FileBehavior.php
+++ FileBehavior.php
@@ -4,97 +4,165 @@
 */
 namespace tpmanc\filebehavior;
 
+use Yii;
 use yii\base\Behavior;
-use yii\web\UploadedFile;
 use yii\base\Event;
-use yii\db\ActiveRecord;
-use app\components\Imagemagick;
+use yii\imagine\Image;
+use yii\db\BaseActiveRecord;
+use yii\web\UploadedFile;
 
 class FileBehavior extends Behavior
 {
-    public $imageFolder = '';
-    public $imageExtension = '';
-    public $imageSizes = [
-        'thumbWidth' => 100,
-        'thumbHeight' => 100,
-        'maxWidth' => 800,
-        'maxHeight' => 800,
-    ];
+    public $fileModel;
+    public $fileLinkModel;
+    public $linkItemColumn = 'itemId';
+    public $linkFileColumn = 'fileId';
+    public $fileFolder = '';
+    public $fileVar;
+    public $fileType;
+    public $imageSizes = false;
+
+    private $file;
+
+    /**
+     * @inheritdoc
+     */
+    // public function init()
+    // {
+    //     parent::init();
+    //     if ($this->attribute === null) {
+    //         throw new InvalidConfigException('The "attribute" property must be set.');
+    //     }
+    //     if ($this->path === null) {
+    //         throw new InvalidConfigException('The "path" property must be set.');
+    //     }
+    //     if ($this->url === null) {
+    //         throw new InvalidConfigException('The "url" property must be set.');
+    //     }
+    // }
 
     public function events()
     {
         return [
-            ActiveRecord::EVENT_BEFORE_INSERT => 'beforeSave',
-            ActiveRecord::EVENT_BEFORE_UPDATE => 'beforeSave',
-            ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
+            BaseActiveRecord::EVENT_BEFORE_VALIDATE => 'beforeValidate',
+            BaseActiveRecord::EVENT_BEFORE_INSERT => 'beforeSave',
+            BaseActiveRecord::EVENT_BEFORE_UPDATE => 'beforeSave',
+            BaseActiveRecord::EVENT_AFTER_INSERT => 'afterSave',
+            BaseActiveRecord::EVENT_AFTER_UPDATE => 'afterSave',
+            BaseActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
         ];
     }
 
-    private function getImagePath()
-    {
-        return [
-            'origin' => $this->getFolderPath() . $this->owner->imageId . '.' . $this->owner->imageExtension,
-            'thumb' => $this->getFolderPath() . $this->owner->imageId . '_thumb.' . $this->owner->imageExtension,
-        ];
-    }
+    // private function getImagePath()
+    // {
+    //     return [
+    //         'origin' => $this->getFolderPath() . $this->owner->imageId . '.' . $this->owner->imageExtension,
+    //         'thumb' => $this->getFolderPath() . $this->owner->imageId . '_thumb.' . $this->owner->imageExtension,
+    //     ];
+    // }
 
-    public function getImageUrl()
-    {
-        return [
-            'origin' => $this->getFolderUrl() . $this->owner->imageId . '.' . $this->owner->imageExtension,
-            'thumb' => $this->getFolderUrl() . $this->owner->imageId . '_thumb.' . $this->owner->imageExtension,
-        ];
-    }
+    // public function getImageUrl()
+    // {
+    //     return [
+    //         'origin' => $this->getFolderUrl() . $this->owner->imageId . '.' . $this->owner->imageExtension,
+    //         'thumb' => $this->getFolderUrl() . $this->owner->imageId . '_thumb.' . $this->owner->imageExtension,
+    //     ];
+    // }
 
     private function getFolderPath()
     {
-        return \Yii::getAlias('@webroot/upload/') . $this->owner->imageFolder . '/';
+        return Yii::getAlias($this->fileFolder) . '/';
     }
 
-    private function getFolderUrl()
-    {
-        return \Yii::getAlias('@web/upload/') . $this->owner->imageFolder . '/';
-    }
+    // private function getFolderUrl()
+    // {
+    //     return \Yii::getAlias('@web/upload/') . $this->owner->imageFolder . '/';
+    // }
 
     private function saveFile($eventName)
     {
-        $file = $this->owner->file;
+        $file = $this->owner->{$this->fileVar};
         $error = false;
-        if( $file !== null ){
-            if($eventName !== 'beforeUpdate' || $this->owner->imageId == 0){
-                $image = new Images();
-                $image->tooltip = '';
-                if( !$image->save() ){
+        if ($file !== null) {
+            $fileModelClass = $this->fileModel;
+            $transaction = $fileModelClass::getDb()->beginTransaction();
+            // save file model
+            $fileModel = new $fileModelClass;
+            $fileModel->image = 'temp';
+            if ($fileModel->save()) {
+                $fileName = $fileModel->id . '.' . $file->extension;
+                $fileModel->image = $fileName;
+                if (!$fileModel->save()) {
                     $error = true;
-                }else{
-                    $this->owner->imageId = $image->id;
+                }
+            } else {
+                $error = true;
+            }
+            if (!$error) {
+                // save file link model
+                $linkModel = new $this->fileLinkModel;
+                $linkModel->{$this->linkFileColumn} = $fileModel->id;
+                $linkModel->{$this->linkItemColumn} = $this->owner->id;
+                if (!$linkModel->save()) {
+                    $error = true;
                 }
             }
-            if( !$error ){
-                $filePath = $this->getFolderPath() . $this->owner->imageId . '.' .$this->owner->imageExtension;
-                $fileThumbPath = $this->getFolderPath() . $this->owner->imageId . '_thumb.' .$this->owner->imageExtension;
-                if ( $file->saveAs($filePath ) ){
-                    // resize
-                    $image = new Imagemagick($filePath);
-                    // $image->resizeImage( $filePath, $this->owner->imageSizes['maxWidth'], $this->owner->imageSizes['maxHeight'] );
-
-                    // thumb
-                    $image = new Imagemagick($filePath);
-                    $image->makeThumb($fileThumbPath, $this->owner->imageSizes['thumbWidth'], $this->owner->imageSizes['thumbHeight']);
+            if (!$error) {
+                // save files
+                if ($this->imageSizes !== false && is_array($this->imageSizes)) {
+                    foreach ($this->imageSizes as $sizeName => $size) {
+                        if (isset($size['folder']) && $size['folder'] !== '') {
+                            $filePath = $this->getFolderPath() . $size['folder'] . '/' . $fileName;
+                        } else {
+                            $filePath = $this->getFolderPath() . '/' . $fileName;
+                        }
+                        if (!$file->saveAs($filePath)) {
+                            $error = true;
+                            break;
+                        } else {
+                            if (isset($size['width']) && isset($size['height'])) {
+                                Image::thumbnail($filePath, $size['width'], $size['height'])
+                                    ->save($filePath, ['quality' => 100]);
+                            }
+                        }
+                    }
+                } else {
+                    $filePath = $this->getFolderPath() . $fileName;
+                    if (!$file->saveAs($filePath)) {
+                        $error = true;
+                    }
                 }
             }
         }
+        if ($error) {
+            $transaction->rollBack();
+        } else {
+            $transaction->commit();
+        }
     }
 
-    private function deleteFile()
+    // private function deleteFile()
+    // {
+    //     $id = $this->owner->imageId;
+    //     $image = Images::findOne($id);
+    //     if ($image !== null) {
+    //         if ($image->delete()) {
+    //             unlink( $this->getImagePath()['origin'] );
+    //             unlink( $this->getImagePath()['thumb'] );
+    //         }
+    //     }
+    // }
+
+    /**
+     * Invoked before validation starts.
+     */
+    public function beforeValidate()
     {
-        $id = $this->owner->imageId;
-        $image = Images::findOne($id);
-        if( $image !== null ){
-            if( $image->delete() ){
-                unlink( $this->getImagePath()['origin'] );
-                unlink( $this->getImagePath()['thumb'] );
-            }
+        /** @var BaseActiveRecord $model */
+        $model = $this->owner;
+        $this->file = UploadedFile::getInstance($model, $this->fileVar);
+        if ($this->file instanceof UploadedFile) {
+            $model->{$this->fileVar} = $this->file;
         }
     }
 
@@ -102,11 +170,16 @@ class FileBehavior extends Behavior
     /* events */
     public function beforeSave($event)
     {
-        $this->saveFile($event->name);
+        // $this->saveFile($event->name);
     }
 
-    public function beforeDelete($event)
+    public function afterSave()
     {
-        $this->deleteFile();
+        $this->saveFile($event);
     }
+
+    // public function beforeDelete($event)
+    // {
+    //     $this->deleteFile();
+    // }
 }
